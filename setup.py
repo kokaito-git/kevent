13a55fda6a68bfc01aa837ad9cb5eb2a27185224
kokaito <kokaito.git@gmail.com>
2025-04-26 04:22:54 +0200

    FIXED LOCKER THINGS

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     author="kokaito-git",
     name="kevent",
-    version="0.1.3.2",
+    version="0.1.3.4",
     description="KEvent",
     author_email="kokaito.git@gmail.com",
     url="https://github.com/kokaito-git/kevent",
